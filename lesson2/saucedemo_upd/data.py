af703d32289d992d84d330f2f678fb7da5a63050
Slava <smikhalitsynge@gmail.com>
2023-12-13 20:28:31 +0100

    pages scheme refactoring

--- lesson2/saucedemo_upd/data.py
+++ lesson2/saucedemo_upd/data.py
@@ -5,3 +5,7 @@ class AuthData:
     INVALID_LOGIN = 'user'
     INVALID_PASSWORD = 'user'
     AUTH_ERROR = 'Epic sadface: Username and password do not match any user in this service'
+
+class InventoryData:
+    item_id = '4'
+    item = 'backpack'
--- lesson2/saucedemo_upd/locators.py
+++ lesson2/saucedemo_upd/locators.py
@@ -1,5 +1,16 @@
-class AuthLocators:
-    _username_field = 'user-name' #id
-    _password_field = 'password' #id
-    _login_button = 'login-button' #id
-    _validation_error = '//h3[@data-test="error"]' #xpath
+# from data import InventoryData
+# from selenium.webdriver.common.by import By
+
+
+# class AuthLocators:
+#     _username_field = (By.ID, 'user-name')
+#     _password_field = (By.ID, 'password')
+#     _login_button = (By.ID, 'login-button')
+#     _validation_error = (By.XPATH, '//h3[@data-test="error"]')
+
+# class InventoryLocators:
+#     _add_to_cart_button = (By.XPATH, f'//button[@data-test="add-to-cart-sauce-labs-{InventoryData.item}"]')
+#     _cart_badge = (By.CLASS_NAME, 'shopping_cart_badge')
+#     _remove_from_cart_button = (By.XPATH, f'//button[@data-test="remove-sauce-labs-{InventoryData.item}"]')
+#     _item_card = (By.ID, f'item_{InventoryData.item_id}_title_link')
+#     _shopping_cart_link = (By.CLASS_NAME, 'shopping_cart_link')
